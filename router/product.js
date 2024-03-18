6954efa416b5af01f52d5a948a7e671357d203a6
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-03-18 10:38:31 +0700

    Sử dụng middleware+Joi để validate dữ liệu

--- router/product.js
+++ router/product.js
@@ -1,34 +1,10 @@
 import express from 'express';
 import { products } from '../models/products.js';
-import Joi from 'joi';
-
-const CheckValidate = Joi.object({
-    name: Joi.string().required().empty().messages({
-        "any.required":"Tên không để trống",
-        "string.empty":"Tên không đúng định dạng"
-    }),
-    image: Joi.string().required().empty().messages({
-        "any.required":"Ảnh không để trống",
-        "string.empty":"Ảnh không đúng định dạng"
-    }),
-    price: Joi.number().required().min(1000).messages({
-        "any.required":"Giá không để trống",
-        "number.min":"Giá sản phẩm không nhỏ hơn 1000"
-    })
-})
+import { CheckProductValidate } from '../middleware/product.js';
 const router = express.Router();
-router.post('/products',async (req,res)=>{
-    const {name,image,price} = req.body; 
-    // console.log(name,image,price);   
-    const {error} = CheckValidate.validate({name,image,price});
-    if (error){
-        // console.log(error);
-        res.send({status:false,message:error.message});
-    }
-    else{
-      const product = await new products(req.body).save();
-    res.send({status:true,data:product});  
-    }
+router.post('/products',CheckProductValidate,async (req,res)=>{
+    const product = await new products(req.body).save();
+    res.send({status:true,data:product}); 
 })
 router.get('/products',async(req,res)=>{
     const response = await products.find();
