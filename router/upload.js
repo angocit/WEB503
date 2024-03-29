a5d1e6b050e0eff845d26c8465639ee976fac788
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-03-29 11:08:34 +0700

    upload ảnh lên cloudinary

--- router/upload.js
+++ router/upload.js
@@ -1,6 +1,16 @@
 import express from 'express';
 import upload from '../models/upload.js'
 import fs from 'fs'
+import cloudinary from 'cloudinary'
+import multer from 'multer';
+import os from 'os'
+cloudinary.v2.config({
+    cloud_name: 'dyzal5ujh',
+    api_key: '989538785622417',
+    api_secret: 'QP7A3WiLWA6PaU6UskIOyVOaaUU',
+    secure: true,
+  });
+const uploadCloud = multer({dest:os.tmpdir()})
 const router = express.Router();
     router.post('/upload',upload.any(),(req,res)=>{
         try {
@@ -17,5 +27,16 @@ const router = express.Router();
         const image = fs.readFileSync(`./uploads/${filename}`)
         res.contentType('image/jpeg')
         res.send(image)
+    })
+    router.post('/upload2',uploadCloud.any(),async (req,res)=>{
+        try {
+        // console.log(req.files);
+        const tmpimg = req.files[0].path
+        const image = await cloudinary.uploader.upload(tmpimg);
+        console.log(image);
+        res.status(200).send({status:true,image:image.url})
+    } catch (error) {
+            
+    }
     })
 export default router
