3623b14b62347fb449bccc88547ae9f6e5519419
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-02-20 19:43:21 +0700

    B14:Upload ảnh

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import ProductRouter from "./routers/product.js"
 import UserRouter from "./routers/auth.js"
+import UploadRouter from "./routers/upload.js"
 const app = express()
 const port = 8000
 app.use(express.json())
 app.use('/api',ProductRouter)
 app.use('/auth',UserRouter)
+app.use('/file',UploadRouter)
 const connectDB = async()=>{
     try {
         await mongoose.connect(`mongodb://localhost:27017/wd19319`)
---
+++ routers/upload.js
@@ -0,0 +1,34 @@
+import express from "express"
+import multer from "multer"
+import fs from "fs"
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {    
+      cb(null, Date.now()+"-"+file.originalname)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+const router = express.Router()
+router.post("/upload",upload.any(),(req,res)=>{
+    try {
+        const filename = req.files[0].filename
+        res.status(200).send({status:true,message:"upload thành công",url:"/file/"+filename})
+    } catch (error) {
+        res.status(500).send({status:false,message:"Upload thất bại"})
+    }
+})
+// Đọc ảnh từ thư mục upload và hiển thị cho ng dùng
+router.get("/:filename",(req,res)=>{
+    try {
+        const filename = req.params.filename
+        const image = fs.readFileSync(`uploads/${filename}`)
+        res.contentType("image/jpeg")
+        res.send(image)
+    } catch (error) {
+        res.status(500).send({status:false,message:error.message})
+    }
+})
+export default router
