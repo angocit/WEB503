dc9ec2a2cfd37288723ed111efd91dd059e5f1c3
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2025-03-15 13:34:36 +0700

    b2: Hiển thị slider bằng react query

--- src/components/slider/HomeSlide.tsx
+++ src/components/slider/HomeSlide.tsx
@@ -5,6 +5,9 @@ import { Settings } from "react-slick";
 import Slider from 'react-slick'
 import { Link } from 'react-router-dom';
 import "./style.css"
+import { useQuery } from '@tanstack/react-query';
+import { ListData } from '../../services/data';
+import { ISlide } from '../../interface/slide';
 const HomeSlide = () => {
     const settings:Settings = {
         dots: true,
@@ -16,34 +19,30 @@ const HomeSlide = () => {
         autoplaySpeed:5000,
         autoplay:true
       };
+      const {data,isLoading} = useQuery<ISlide[]>({
+        queryKey:["sliders"],
+        queryFn:async()=>{
+            const {data} = await ListData("sliders")
+            return data
+        }
+      })
   return (
     <div className="slider-container">
+      {(isLoading)?<>Loading...</>:
       <Slider {...settings}>
-        <div className='relative'>
-          <img src={"https://feelingteaonline.com/wp-content/uploads/2023/07/zalo-3.jpg"} />
-            <div className='content'>
-                <label>Label 1</label>
-                <h3>Trà sữa TLINK, uống là thích</h3>
-                <Link to={'#'}>Mua ngay</Link>
-            </div>
-        </div>
-        <div className='relative'>
-          <img src={"https://cdn-www.vinid.net/2020/05/be195fb4-20200501_appvinid_bannerweb_toocha.jpg"} />
-          <div className='content'>
-                <label>Label 2</label>
-                <h3>Trà sữa TLINK, uống là thích 2</h3>
-                <Link to={'#'}>Mua ngay</Link>
-            </div>
-        </div>
-        <div className='relative'>
-          <img src={"https://yihetang.com.vn/public/upload/1.jpg"} />
-          <div className='content'>
-                <label>Label 3</label>
-                <h3>Trà sữa TLINK, uống là thích 3</h3>
-                <Link to={'#'}>Mua ngay</Link>
-            </div>
-        </div>
+        { (data)&&data.map(item=>(
+          <div className='relative' key={item.id}>
+            <img src={item.image} />
+              <div className='content'>
+                  <label>{item.label}</label>
+                  <h3>{item.caption}</h3>
+                  <Link className='bg-red-700 px-4 py-2 rounded text-white max-w-[120px] mx-auto uppercase font-bold' to={item.url}>Mua ngay</Link>
+              </div>
+          </div>
+          ))
+        }
       </Slider>
+        }
     </div>
   )
 }
